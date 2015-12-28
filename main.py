dc023c7ebbcd3dc362bbd9d5ab65e4efab6b1cb0
Stepa Alimov <stepa.alimov.93@gmail.com>
2015-12-29 00:20:49 +0300

    pmgai Unit#2 Beginner + Intermediate

--- main.py
+++ main.py
@@ -3,17 +3,16 @@
 # Copyright (c) 2015, AiGameDev.com KG.
 #
 
-import os                                       # Built-in Python modules.
+import os  # Built-in Python modules.
 import sys
 import math
 import time
 import random
 
 sys.path.append('pymcworldgen')
-import layer                                    # Modules from pymcworldgen.
+import layer  # Modules from pymcworldgen.
 import saveutils
 import constants as c
-
 
 
 class DistanceField(object):
@@ -27,20 +26,38 @@ class DistanceField(object):
         
         y (int) represents altitude, positive is up and negative is down.
         x and z (int) are the lattitude and logitude in the map. 
-        """ 
+        """
+
+        step = 70
+
+        sx = 1
+        if (x < 0):
+            sx = -sx
+
+        sz = 1
+        if (z < 0):
+            sz = -sz
 
         # Add simple noise to the coordinates to make it less rigid!
-        nx = x + 2*math.sin(z/4.7)
-        nz = z + 2*math.cos(x/5.9)
-        ny = y + math.sin(0.75 + x/7.8 + z/9.1)
+        fx = math.fmod(x, step) - sx * step / 2
+        fz = math.fmod(z, step) - sz * step / 2
+        nx = fx + 2 * math.sin(z / 4.7 + math.cos(fx))
+        nz = fz + 2 * math.cos(x / 5.9 + math.sin(fz))
+        if z != 0:
+            nx += math.cos(math.fmod(x * x, z))
+        if x != 0:
+            nz += math.sin(math.fmod(z * z, x))
+        ny = y + math.sin(0.75 + math.cos(x / 7.8) + math.sin(z / 9.1))
 
-        # Manhattan distance field to center of map: floating iceberg.
-        if abs(nx) + abs(nz) + abs(ny*8) < 64:
-            return c.MAT_SNOW
+        if math.sqrt(nx * nx + nz * nz + ny * ny * 64) < 32:
+            return c.MAT_DIRT
 
         # Anything that's not solid but under the sea level?
-        if y < 0:
+        if y < -10:
             return c.MAT_WATER
+
+        if y == 0:
+            return c.MAT_SNOW
 
         return c.MAT_AIR
 
@@ -52,13 +69,13 @@ class DistanceField(object):
         """
 
         WIDTH = c.CHUNK_WIDTH_IN_BLOCKS
-        HALF_HEIGHT = c.CHUNK_HEIGHT_IN_BLOCKS//2
+        HALF_HEIGHT = c.CHUNK_HEIGHT_IN_BLOCKS // 2
 
         chunk = layer.Chunk(cx, cz)
-        for x in range(cx * WIDTH, (cx+1) * WIDTH):
-            for z in range(cz * WIDTH, (cz+1) * WIDTH):
-                for y in range(-HALF_HEIGHT, +HALF_HEIGHT): 
-                    chunk.blocks[x%WIDTH][z%WIDTH][y+HALF_HEIGHT] = self.get_block(x, z, y)
+        for x in range(cx * WIDTH, (cx + 1) * WIDTH):
+            for z in range(cz * WIDTH, (cz + 1) * WIDTH):
+                for y in range(-HALF_HEIGHT, +HALF_HEIGHT):
+                    chunk.blocks[x % WIDTH][z % WIDTH][y + HALF_HEIGHT] = self.get_block(x, z, y)
         return chunk
 
 
@@ -81,7 +98,7 @@ def generate():
         for chunkcol in range(-worldsizez, worldsizez):
             currchunk = pipeline.getChunk(chunkrow, chunkcol)
             saveutils.setWorldChunk(island, currchunk, chunkrow, chunkcol)
-    
+
     saveutils.saveWorld(island)
     print("Processing took", start_time - time.clock(), "seconds.")
 
